47f00f52f47cefe6893de389d77b57a4fdc643dc
DevExpressExampleBot <fakeEmail>
2021-10-14 11:29:00 +0300

    autogenerate VB

--- VB/WinForms_Dashboard_BeforeExportEvent_Example/Form1.vb
+++ VB/WinForms_Dashboard_BeforeExportEvent_Example/Form1.vb
@@ -1,44 +1,46 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWin
 Imports System
 
 Namespace WinForms_Dashboard_BeforeExportEvent_Example
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraEditors.XtraForm
 
         Public Sub New()
             InitializeComponent()
             dashboardDesigner1.CreateRibbon()
-            AddHandler dashboardDesigner1.ConfigureDataConnection, AddressOf DashboardDesigner1_ConfigureDataConnection
-            AddHandler dashboardDesigner1.CustomizeDashboardTitle, AddressOf DashboardDesigner1_CustomizeDashboardTitle
+            Me.dashboardDesigner1.ConfigureDataConnection += AddressOf DashboardDesigner1_ConfigureDataConnection
+            Me.dashboardDesigner1.CustomizeDashboardTitle += AddressOf DashboardDesigner1_CustomizeDashboardTitle
             dashboardDesigner1.LoadDashboard("Data\Dashboard2Print.xml")
-            AddHandler dashboardDesigner1.BeforeExportDocument, AddressOf DashboardDesigner1_BeforeExportDocument
+            Me.dashboardDesigner1.BeforeExportDocument += AddressOf DashboardDesigner1_BeforeExportDocument
         End Sub
 
         Private Sub DashboardDesigner1_CustomizeDashboardTitle(ByVal sender As Object, ByVal e As CustomizeDashboardTitleEventArgs)
-            Dim btn As DashboardToolbarItem = New DashboardToolbarItem()
-            Dim action As Action(Of DashboardToolbarItemClickEventArgs) =
-                Sub()
-                    dashboardDesigner1.ExportToPdf("test.pdf")
-                    System.Diagnostics.Process.Start("test.pdf")
-                End Sub
-            btn.ClickAction = action
-            btn.Caption = "Export to PDF"
-            btn.SvgImage = DevExpress.Utils.Svg.SvgImage.FromFile("Action_Export_ToPDF.svg")
-            e.Items.Add(btn)
+            e.Items.Add(New DashboardToolbarItem() With {.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(Sub(args)
+                dashboardDesigner1.ExportToPdf("test.pdf")
+                System.Diagnostics.Process.Start("test.pdf")
+            End Sub), .Caption = "Export to PDF", .SvgImage = DevExpress.Utils.Svg.SvgImage.FromFile("Action_Export_ToPDF.svg")})
         End Sub
 
         Private Sub DashboardDesigner1_BeforeExportDocument(ByVal sender As Object, ByVal e As BeforeExportDocumentEventArgs)
-            If e.ExportAction = DashboardExportAction.ExportToPdf AndAlso e.PdfExportOptions.ExportFilters = False Then
+            If e.ExportAction Is DashboardExportAction.ExportToPdf AndAlso e.PdfExportOptions.ExportFilters = False Then
                 e.HideItem(Function(item) TypeOf item Is FilterElementDashboardItem OrElse TypeOf item Is DateFilterDashboardItem)
             End If
         End Sub
 
         Private Sub DashboardDesigner1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DevExpress.DashboardCommon.DashboardConfigureDataConnectionEventArgs)
-            If TypeOf e.ConnectionParameters Is ExtractDataSourceConnectionParameters Then
-                Dim extractParams As ExtractDataSourceConnectionParameters = DirectCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
-                extractParams.FileName = "Data\SalesPerson.dat"
-            End If
+            Dim extractParams As ExtractDataSourceConnectionParameters = Nothing
+            If CSharpImpl.__Assign(extractParams, TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)) IsNot Nothing Then extractParams.FileName = "Data\SalesPerson.dat"
         End Sub
+
+        Private Class CSharpImpl
+
+            <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
     End Class
 End Namespace
